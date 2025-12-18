7f6b1cfd8087b6b29d63831c1cfb383261576973
Tusharc11 <87057674+Tusharc11@users.noreply.github.com>
2025-12-18 23:07:52 +0530

    Create CartClient.tsx

---
+++ src/app/cart/CartClient.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { CartItemCard } from "core/components/cart-item-card/cart-item-card";
+import { Summary } from "core/components/summary/summary";
+import styles from "./page.module.scss";
+
+export default function CartClient({ products }) {
+  return products?.length > 0 ? (
+    <>
+      <div className={styles["items"]}>
+        {products.map((p) => (
+          <CartItemCard
+            key={p.product.id}
+            product={p.product}
+            quantity={p.quantity}
+          />
+        ))}
+      </div>
+
+      <div className={styles["summary"]}>
+        <Summary />
+      </div>
+    </>
+  ) : (
+    <h1>Empty cart</h1>
+  );
+}
