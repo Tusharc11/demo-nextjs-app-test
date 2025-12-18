98df23a8796395914ae7c1ea860bdb054ea252d1
Tusharc11 <87057674+Tusharc11@users.noreply.github.com>
2025-12-18 23:06:44 +0530

    Update page.tsx

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,35 +1,15 @@
 export const dynamic = "force-dynamic";
-"use client";
 
-import { CartItemCard } from "core/components/cart-item-card/cart-item-card";
 import styles from "./page.module.scss";
-import { Summary } from "core/components/summary/summary";
-import { useCart } from "../../hooks/useCart/useCart";
+import CartClient from "./CartClient";
+import { getCartProducts } from "./serverCart";
 
 export default async function Page() {
-  const { getCartProducts } = useCart();
   const products = await getCartProducts();
 
   return (
     <div className={styles["container"]}>
-      {products?.length > 0 ? (
-        <>
-          <div className={styles["items"]}>
-            {products.map((p) => (
-              <CartItemCard
-                key={p.product.id}
-                product={p.product}
-                quantity={p.quantity}
-              />
-            ))}
-          </div>
-          <div className={styles["summary"]}>
-            <Summary />
-          </div>
-        </>
-      ) : (
-        <h1> Empty cart </h1>
-      )}
+      <CartClient products={products} />
     </div>
   );
 }
